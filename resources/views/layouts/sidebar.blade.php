7f21dbdb743b91321dfbb1cccdc12645ffb4f44a
rezadika11 <rezadika11@gmail.com>
2022-08-18 17:18:21 +0700

    dashboard sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
       <div class="sidebar-brand">
-        <a href="index.html">PEMETAAN PARKIR</a>
+        <a href="{{ route('dashboard') }}">PEMETAAN PARKIR</a>
       </div>
       <div class="sidebar-brand sidebar-brand-sm">
         <a href="index.html">SIG</a>
