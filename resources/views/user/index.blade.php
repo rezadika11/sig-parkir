456b53124773fe4e332d4722515f6d29b91c4f97
rezadika11 <rezadika11@gmail.com>
2022-08-18 17:40:51 +0700

    logo icon

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,6 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>SIG Parkir</title>
 
+    <link rel="icon" href="/assets/img/logo-kab.png">
+
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
